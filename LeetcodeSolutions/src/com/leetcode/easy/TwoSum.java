04e763fcdeeb7f484f4ac92ea1ad00dc703f3ed3
EthanLev <ethancolemanlevesque@gmail.com>
2025-10-16 19:21:50 -0600

    docs: add TwoSum solution

---
+++ LeetcodeSolutions/src/com/leetcode/easy/TwoSum.java
@@ -0,0 +1,35 @@
+package com.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+	// Attempt 1
+//	public int[] twoSum(int[] nums, int target) {
+//        int n = nums.length;
+//        for (int i = 0; i < n - 1; i++) {
+//            for (int j = i + 1; j < n; j++) {
+//                if (nums[i] + nums[j] == target) {
+//                    return new int[]{i, j};
+//                }
+//            }
+//        }
+//        return new int[]{}; // No solution found
+//    }
+	
+	// Attempt 2	2ms
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> complements = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			Integer complementIndex = complements.get(nums[i]);
+			
+			if (complementIndex != null) {
+				return new int[] {i, complementIndex};
+			}
+			
+			complements.put(target - nums[i], i);
+		}
+		
+		return nums;
+	}
+}
